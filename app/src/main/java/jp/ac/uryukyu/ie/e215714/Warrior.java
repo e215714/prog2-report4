b5f5e0ee9bd1f53d9b60aceaa554c7474573ad67
taigao <e215714@ie.u-ryukyu.ac.jp>
2021-12-06 17:47:21 +0900

    fix Warrior

--- app/src/main/java/jp/ac/uryukyu/ie/e215714/Warrior.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e215714/Warrior.java
@@ -7,7 +7,7 @@ public class Warrior extends Hero{
 
     public void attackWithWeponSkill(LivingThing opponent) {
         var damage = (int)(attack*1.5);
+        System.out.printf("%sの攻撃！ウェポンスキルを発動！%sに%dのダメージを与えた！！\n", name, opponent.getName(), damage);
         opponent.wounded(damage);
-        System.out.printf("%sの攻撃！ウェポンスキルを発動！%sに%dのダメージを与えた！！", name, opponent.getName(), damage);
     }
 }
